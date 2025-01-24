7f93522e93faea35cb3f9e34f0596bdc36f71c48
rabbyxq <rabbyxq@gmail.com>
2025-01-25 03:02:19 +0600

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,30 @@
+#include <jni.h>
+#include <string>
+#include <iostream>
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_rabbyx_andro_1myelitis_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_rabbyx_andro_1myelitis_MainActivity_receiveDataFromJS(
+        JNIEnv* env,
+        jobject /* this */,
+        jstring message) {
+
+    // Convert the Java string to a C++ string
+    const char* nativeMessage = env->GetStringUTFChars(message, 0);
+
+    // Print the message to the console
+    std::cout << "Received from JavaScript: " << nativeMessage << std::endl;
+
+    // Release the string once we're done with it
+    env->ReleaseStringUTFChars(message, nativeMessage);
+
+    // Return a response to Java/Kotlin
+    std::string response = "Message received: " + std::string(nativeMessage);
+    return env->NewStringUTF(response.c_str());
+}
---
+++ app/src/main/java/com/rabbyx/andro_myelitis/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.rabbyx.andro_myelitis
+
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import com.rabbyx.andro_myelitis.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set up the WebView
+        val webView: WebView = binding.webView
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(JSInterface(), "Android")
+
+        // Load a sample HTML or JavaScript
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    // Create the interface for JavaScript to call Android
+    class JSInterface {
+        @JavascriptInterface
+        fun sendDataToNative(message: String) {
+            // Call the C++ function or handle the message
+            val response = receiveDataFromJNI(message)
+            println("Received from JavaScript: $message")
+        }
+
+    }
+    external fun receiveDataFromJNI(message: String): String
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'andro_myelitis' library on application startup.
+        init {
+            System.loadLibrary("andro_myelitis")
+        }
+    }
+}
